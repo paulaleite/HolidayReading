a3602ee6f4d3f37b0316ac05f230dcfafa57048a
Paula Leite <paulatleite@Paulas-MacBook-Pro-2.local>
2019-07-09 18:35:58 -0300

    Added sections to TableViewController

--- HolidayReading/HolidayReading/NewBookTVC.swift
+++ HolidayReading/HolidayReading/NewBookTVC.swift
@@ -12,6 +12,10 @@ class NewBookTVC: UITableViewController {
 
     var selectedRow: Int = 0
     
+    var selectedSection: Int = 2
+    
+    var selected: Bool = false
+    
     @IBOutlet weak var bookImageView: UIImageView!
     
     @IBOutlet weak var nameOfBookLabel: UILabel!
@@ -30,8 +34,6 @@ class NewBookTVC: UITableViewController {
     
     @IBOutlet weak var amountOfPagesPerDayInputCell: UITableViewCell!
     
-    @IBOutlet weak var amountOfPagesPerDayButton: UIButton!
-    
     @IBOutlet weak var amountOfReadingPerDayLabel: UILabel!
     
     @IBOutlet weak var amountOfReadingPerDayTextField: UILabel!
@@ -39,8 +41,6 @@ class NewBookTVC: UITableViewController {
     @IBOutlet weak var amountOfReadingPerDayPicker: UIPickerView!
     
     @IBOutlet weak var amountOfReadinPerDayInputCell: UITableViewCell!
-
-    @IBOutlet weak var amountOfReadingPerDayButton: UIButton!
     
     @IBOutlet weak var timeOfReadingLabel: UILabel!
     
@@ -50,8 +50,6 @@ class NewBookTVC: UITableViewController {
     
     @IBOutlet weak var timeOfReadingInputCell: UITableViewCell!
     
-    @IBOutlet weak var timeOfReadingButton: UIButton!
-    
     @IBOutlet weak var limitForReadingLabel: UILabel!
     
     @IBOutlet weak var limitForReadingTextField: UILabel!
@@ -60,32 +58,11 @@ class NewBookTVC: UITableViewController {
     
     @IBOutlet weak var limitForReadingInputCell: UITableViewCell!
     
-    @IBOutlet weak var limitForReadingButton: UIButton!
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        amountOfPagesPerDayButton.addTarget(self, action: #selector(expandCloseCell), for: .touchUpInside)
-        amountOfPagesPerDayButton.tag = selectedRow
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        selectedRow = indexPath.row
-        
-//        if selectedRow == 2 {
-//            amountOfPagesPerDayInputCell.isHidden.toggle()
-//            self.tableView.moveRow(at: IndexPath(row: 4, section: 0), to: IndexPath(row: 3, section: 0))
-//            self.tableView.moveRow(at: IndexPath(row: 5, section: 0), to: IndexPath(row: 4, section: 0))
-//            self.tableView.moveRow(at: IndexPath(row: 6, section: 0), to: IndexPath(row: 5, section: 0))
-//            self.tableView.moveRow(at: IndexPath(row: 7, section: 0), to: IndexPath(row: 6, section: 0))
-//            self.tableView.moveRow(at: IndexPath(row: 8, section: 0), to: IndexPath(row: 7, section: 0))
-//            self.tableView.moveRow(at: IndexPath(row: 9, section: 0), to: IndexPath(row: 8, section: 0))
-//
-//        }
+        amountOfPagesPerDayInputCell.isHidden = true
     }
     
-    @objc func expandCloseCell() {
-        print("Testing button pressed")
-    }
 
 }
